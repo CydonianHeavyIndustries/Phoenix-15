f90ae7c1f3415617827da8eeea2394d0695998a5
Beurkson <cydonianheavyindustries@gmail.com>
2026-02-11 12:18:57 -0500

    Disable live-build security repo to avoid bookworm/updates error

--- os/docker/build.sh
+++ os/docker/build.sh
@@ -40,6 +40,7 @@ lb config \
   --architectures amd64 \
   --binary-images iso-hybrid \
   --archive-areas "main contrib non-free-firmware" \
+  --security false \
   --mirror-bootstrap "http://deb.debian.org/debian" \
   --mirror-binary "http://deb.debian.org/debian" \
   --mirror-chroot "http://deb.debian.org/debian" \
